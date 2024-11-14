1aea6abdf6062eabcd45491018b8456dad672be9
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-11-14 18:48:47 +0300

    Created of UnitOfWork

---
+++ Infrastructure/GreenDelight.Persistence/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,25 @@
+﻿using GreenDelight.Apllication.Interfaces.UnitofWorks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenDelight.Persistence.UnitOfWorks
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext dbContext;
+
+        public UnitOfWork(AppDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+
+        public async ValueTask DisposeAsync() => await dbContext.DisposeAsync();
+        public async Task<int> SaveAsync() => await dbContext.SaveChangesAsync();
+
+
+    }
+}
