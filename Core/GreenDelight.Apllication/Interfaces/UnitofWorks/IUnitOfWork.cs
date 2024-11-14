94145a86a38d1201fb536d95e291fdf5ecbaccc2
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-11-14 17:59:58 +0300

    Created of IUnitofWork

---
+++ Core/GreenDelight.Apllication/Interfaces/UnitofWorks/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using GreenDelight.Apllication.Interfaces.Repositories;
+using GreenDelight.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenDelight.Apllication.Interfaces.UnitofWorks
+{
+    public interface IUnitOfWork : IAsyncDisposable
+    {
+        IBaseRepository<T> GetGenericRepository<T>() where T : class, IEntityBase, new();
+
+        Task CommitAsync();
+    }
+}
