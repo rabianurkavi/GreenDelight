cd9f390e0b0c2708e42b070e1d2691d18b809941
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-11-14 17:58:56 +0300

    IEntity renamed

---
+++ Core/GreenDelight.Domain/Abstract/IEntityBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenDelight.Domain.Abstract
+{
+    public interface IEntityBase
+    {
+        public int ID { get; set; }
+    }
+}
