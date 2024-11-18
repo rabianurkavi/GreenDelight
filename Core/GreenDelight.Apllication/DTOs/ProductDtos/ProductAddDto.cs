baf3f418950c3f61229c4259ebe7be2fbdd5556c
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-11-18 23:35:57 +0300

    Added ProductDto

---
+++ Core/GreenDelight.Apllication/DTOs/ProductDtos/ProductAddDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenDelight.Apllication.DTOs.ProductDtos
+{
+    public class ProductAddDto
+    {
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public int CategoryID { get; set; }
+        public decimal Price { get; set; }
+        public bool InStock { get; set; }
+        public string ImageUrls { get; set; }
+    }
+}
---
+++ Core/GreenDelight.Apllication/DTOs/ProductDtos/ProductDetailDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenDelight.Apllication.DTOs.ProductDtos
+{
+    public class ProductDetailDto
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public bool InStock { get; set; }
+        public string ImageUrls { get; set; }
+        public string CategoryName { get; set; } // Category ilişkisi için
+        public DateTime CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+    }
+}
