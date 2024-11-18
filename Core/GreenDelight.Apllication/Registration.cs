079169ad5808fb3771a8c0ff4f27289a5ec64065
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-11-19 02:49:20 +0300

    registration added to application layer

---
+++ Core/GreenDelight.Apllication/Registration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenDelight.Apllication
+{
+    public static class Registration
+    {
+        public static void AddAplication(this IServiceCollection services)
+        {
+        }
+    }
+}
