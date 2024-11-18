1bf754378979957ba94e50fc2b4ccdc917975ee4
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-11-19 02:47:54 +0300

    result pattern added to ProductService

---
+++ Core/GreenDelight.Apllication/Interfaces/Services/ProductServices/IProductService.cs
@@ -0,0 +1,19 @@
+﻿using GreenDelight.Apllication.DTOs.ProductDtos;
+using GreenDelight.Domain.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenDelight.Application.Interfaces.Services.ProductServices
+{
+    public interface IProductService
+    {
+        Task<IResult> AddAsync(ProductAddDto productAddDto);
+        Task<IResult> UpdateAsync(ProductUpdateDto productUpdateDto);
+        Task<IDataResult<ProductDto>> GetByIdAsync(int id);
+        Task<IDataResult<List<ProductDetailDto>>> GetAllAsync();
+        Task<IResult> RemoveAsync(int id);
+    }
+}
