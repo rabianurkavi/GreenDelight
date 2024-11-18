a25722cf8cbbd372c32c96cc4387e20aff6efaad
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-11-19 02:46:24 +0300

    Added result pattern

---
+++ Core/GreenDelight.Domain/Results/ErrorDataResult.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenDelight.Domain.Results
+{
+    public class ErrorDataResult<T> : DataResult<T>
+    {
+        public ErrorDataResult(T data, bool success, string message) : base(data, false, message)
+        {
+
+        }
+        public ErrorDataResult(T data, bool succes) : base(data, false)
+        {
+
+        }
+        public ErrorDataResult(string message) : base(message)
+        {
+
+        }
+    }
+}
---
+++ Core/GreenDelight.Domain/Results/SuccessResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenDelight.Domain.Results
+{
+    public class SuccessResult : Result
+    {
+        public SuccessResult(string message) : base(true, message)
+        {
+            //Sadece mesaj
+        }
+        public SuccessResult() : base(true)
+        {
+
+        }
+    }
+}
