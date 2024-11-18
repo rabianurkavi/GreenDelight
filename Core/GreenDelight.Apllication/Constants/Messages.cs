2575ae261cbe77cf1f57aab8d544f171359654d4
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-11-19 02:46:40 +0300

    Created constants message

---
+++ Core/GreenDelight.Apllication/Constants/Messages.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenDelight.Application.Constants
+{
+    public class Messages
+    {
+        public static string ProductAdded = "Ürün eklendi";
+        public static string ProductDeleted = "Ürün silindi";
+        public static string ProductUpdated = "Ürün güncellendi";
+        public static string MaintenanceTime = "Zaman hatası";
+        public static string ProductInvalid = "Ürün ismi geçersiz";
+        public static string ProductsListed = "Ürünler listelendi";
+    }
+}
