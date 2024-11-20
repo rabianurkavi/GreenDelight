b5cebffa5b9334c3086f1aeb565f5f9b9bef0c7d
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-11-20 16:22:35 +0300

    Created CategoryService

---
+++ Core/GreenDelight.Apllication/Interfaces/Services/CategoryServices/ICategoryService.cs
@@ -0,0 +1,20 @@
+﻿using GreenDelight.Apllication.DTOs.ProductDtos;
+using GreenDelight.Application.DTOs.CategoryDtos;
+using GreenDelight.Domain.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenDelight.Application.Interfaces.Services.CategoryServices
+{
+    public interface ICategoryService
+    {
+        Task<IResult> AddAsync(CategoryAddDto categoryAddDto);
+        Task<IResult> UpdateAsync(CategoryDetailDto categoryDetailDto);
+        Task<IDataResult<CategoryDetailDto>> GetByIdAsync(int id);
+        Task<IDataResult<List<CategoryDetailDto>>> GetAllAsync();
+        Task<IResult> RemoveAsync(int id);
+    }
+}
