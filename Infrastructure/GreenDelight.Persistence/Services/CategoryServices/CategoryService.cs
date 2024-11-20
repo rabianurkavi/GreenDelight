46665953f1ea3ef3c6404acddb2a8d5ff959b980
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-11-20 16:22:43 +0300

    Created CategoryService

--- Infrastructure/GreenDelight.Persistence/Services/CategoryServices/CategoryService.cs
+++ Infrastructure/GreenDelight.Persistence/Services/CategoryServices/CategoryService.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿using GreenDelight.Apllication.DTOs.ProductDtos;
+using GreenDelight.Apllication.Interfaces.UnitofWorks;
+using GreenDelight.Application.Constants;
+using GreenDelight.Application.DTOs.CategoryDtos;
+using GreenDelight.Application.Interfaces.Services.CategoryServices;
+using GreenDelight.Domain.Concrete;
+using GreenDelight.Domain.Results;
+using Mapster;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +14,95 @@ using System.Threading.Tasks;
 
 namespace GreenDelight.Persistence.Services.CategoryServices
 {
-    public class CategoryService
+    public class CategoryService : ICategoryService
     {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CategoryService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<IResult> AddAsync(CategoryAddDto categoryAddDto)
+        {
+            if (categoryAddDto == null)
+            {
+                return new ErrorResult("Kategori bilgileri eksik.");
+            }
+
+            var category = categoryAddDto.Adapt<Category>();
+            await _unitOfWork.GetGenericRepository<Category>().AddAsync(category);
+            await _unitOfWork.CommitAsync();
+
+            return new SuccessResult(Messages.CategoryAdded);
+        }
+
+        public async Task<IDataResult<List<CategoryDetailDto>>> GetAllAsync()
+        {
+            var categories = await _unitOfWork.GetGenericRepository<Category>().GetAllAsync();
+
+            if (categories == null || !categories.Any())
+            {
+                return new ErrorDataResult<List<CategoryDetailDto>>("Kategori bulunamadı.");
+            }
+
+            var categoriesDto = categories.Adapt<List<CategoryDetailDto>>();
+            return new SuccessDataResult<List<CategoryDetailDto>>(categoriesDto, Messages.CategorysListed);
+        }
+
+        public async Task<IDataResult<CategoryDetailDto>> GetByIdAsync(int id)
+        {
+            if (id <= 0)
+            {
+                return new ErrorDataResult<CategoryDetailDto>("Geçersiz kategori ID.");
+            }
+
+            var category = await _unitOfWork.GetGenericRepository<Category>().GetAsync(x => x.ID == id);
+            if (category == null)
+            {
+                return new ErrorDataResult<CategoryDetailDto>("Kategori bulunamadı.");
+            }
+
+            var categoryDto = category.Adapt<CategoryDetailDto>();
+            return new SuccessDataResult<CategoryDetailDto>(categoryDto, Messages.CategorysListed);
+        }
+
+        public async Task<IResult> RemoveAsync(int id)
+        {
+            if (id <= 0)
+            {
+                return new ErrorResult("Geçersiz kategori ID.");
+            }
+
+            var category = await _unitOfWork.GetGenericRepository<Category>().GetAsync(x => x.ID == id);
+            if (category == null)
+            {
+                return new ErrorResult("Silinecek kategori bulunamadı.");
+            }
+
+            await _unitOfWork.GetGenericRepository<Category>().DeleteAsync(category);
+            await _unitOfWork.CommitAsync();
+
+            return new SuccessResult(Messages.CategoryDeleted);
+        }
+
+        public async Task<IResult> UpdateAsync(CategoryDetailDto categoryDetailDto)
+        {
+            if (categoryDetailDto == null)
+            {
+                return new ErrorResult("Kategori bilgileri eksik.");
+            }
+
+            var category = await _unitOfWork.GetGenericRepository<Category>().GetAsync(x => x.ID == categoryDetailDto.ID);
+            if (category == null)
+            {
+                return new ErrorResult("Güncellenecek kategori bulunamadı.");
+            }
+            category = categoryDetailDto.Adapt(category);
+
+            await _unitOfWork.GetGenericRepository<Category>().UpdateAsync(category);
+            await _unitOfWork.CommitAsync();
+
+            return new SuccessResult(Messages.CategoryUpdated);
+        }
     }
 }
