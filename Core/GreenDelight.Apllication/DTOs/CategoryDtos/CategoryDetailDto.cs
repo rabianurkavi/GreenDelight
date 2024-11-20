76fbfacfc4f1a5e1b1045f7340e37af25b666249
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-11-20 16:21:58 +0300

    Created CategoryDto

---
+++ Core/GreenDelight.Apllication/DTOs/CategoryDtos/CategoryDetailDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenDelight.Application.DTOs.CategoryDtos
+{
+    public class CategoryDetailDto
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
