eacec569f92ebec882cb6ee09ddf8b92e0b9284c
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-11-22 01:24:08 +0300

    Category controller created

---
+++ Presentation/GreenDelight.API/Controllers/CategoryController.cs
@@ -0,0 +1,68 @@
+﻿using GreenDelight.Apllication.DTOs.ProductDtos;
+using GreenDelight.Application.DTOs.CategoryDtos;
+using GreenDelight.Application.Interfaces.Services.CategoryServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GreenDelight.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddCategory([FromBody] CategoryAddDto categoryAddDto)
+        {
+            var result = await _categoryService.AddAsync(categoryAddDto);
+
+            if (result.Success)
+            {
+                return Ok(result.Message);
+            }
+
+            return BadRequest(result.Message);
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetList()
+        {
+
+            try
+            {
+                // Ürünü ekle
+                var response = await _categoryService.GetAllAsync();
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                // Hata mesajını döndür
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateCategory([FromBody] CategoryDetailDto categoryDetailDto)
+        {
+            var result = await _categoryService.UpdateAsync(categoryDetailDto);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result.Message);
+        }
+        [HttpDelete]
+        public async Task<IActionResult> DeleteCategory(int id)
+        {
+            var result = await _categoryService.RemoveAsync(id);
+            if (result.Success)
+            {
+                return Ok(result.Message);
+            }
+            return BadRequest(result.Message);
+
+        }
+    }
+}
