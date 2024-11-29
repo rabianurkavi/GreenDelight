9e3c43384d9eb70aa516b786b33a50063b73f1fb
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-11-29 19:10:06 +0300

    Created Auth of exceptions

---
+++ Core/GreenDelight.Apllication/Rules/Exceptions/AuthExceptions/EmailAddressShouldBeValidException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenDelight.Application.Rules.Exceptions.AuthExceptions
+{
+    public class EmailAddressShouldBeValidException : BaseException
+    {
+        public EmailAddressShouldBeValidException() : base("Geçersiz e-posta adresi.") { }
+    }
+}
---
+++ Core/GreenDelight.Apllication/Rules/Exceptions/AuthExceptions/RefreshTokenShouldNotBeExpiredException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenDelight.Application.Rules.Exceptions.AuthExceptions
+{
+    public class RefreshTokenShouldNotBeExpiredException : BaseException
+    {
+        public RefreshTokenShouldNotBeExpiredException() : base("Oturum süresi sona ermiştir. Lütfen tekrar giriş yapın.") { }
+
+    }
+}
