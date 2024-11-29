34a8e00b8ccee8aa715ede5aff448a7a8491e3d1
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-11-29 19:09:47 +0300

    additions were made for mapping

--- Core/GreenDelight.Apllication/Mapping/MapsterConfig.cs
+++ Core/GreenDelight.Apllication/Mapping/MapsterConfig.cs
@@ -1,6 +1,10 @@
 ﻿using GreenDelight.Apllication.DTOs.ProductDtos;
+using GreenDelight.Apllication.DTOs.UserDtos;
+using GreenDelight.Application.DTOs.AuthDtos.LoginDtos;
+using GreenDelight.Application.DTOs.AuthDtos.RegisterDtos;
 using GreenDelight.Domain.Concrete;
 using Mapster;
+using Microsoft.AspNetCore.Identity.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -29,6 +33,19 @@ namespace GreenDelight.Apllication.Mapping
             TypeAdapterConfig<Product, ProductDto>.NewConfig()
             .Map(dest => dest.CategoryName, src => src.Category.Name);
 
+            TypeAdapterConfig<User, UserAddDto>.NewConfig();
+            TypeAdapterConfig<User, UserDetailDto>.NewConfig();
+            TypeAdapterConfig<User, UserDto>.NewConfig();
+            TypeAdapterConfig<User, UserUpdateDto>.NewConfig();
+
+            TypeAdapterConfig<User, LoginDto>.NewConfig()
+                .Map(dest => dest.Email, src => src.Email)
+                .Map(dest => dest.Password, src => src.PasswordHash);
+
+            TypeAdapterConfig<RegisterDto, User>.NewConfig()
+                .Map(dest => dest.FullName, src => src.FullName)
+                .Map(dest => dest.Email, src => src.Email)
+                .Map(dest => dest.UserName, src => src.Email); // Email'i UserName olarak ayarlama
         }
     }
 }
