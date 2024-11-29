9568d1248b642de96041373da118960bdada0127
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-11-29 19:13:55 +0300

    done dependency injection

---
+++ Infrastructure/GreenDelight.Infrastructure/Registration.cs
@@ -0,0 +1,45 @@
+﻿using GreenDelight.Application.Interfaces.Token;
+using GreenDelight.Infrastructure.Tokens;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenDelight.Infrastructure
+{
+    public static class Registration
+    {
+        public static void AddInfrastructure(this IServiceCollection services,IConfiguration configuration)
+        {
+            services.Configure<TokenSettings>(configuration.GetSection("JWT"));
+            services.AddTransient<ITokenService, TokenService>();
+
+            services.AddHttpClient();
+            services.AddAuthentication(opt =>
+            {
+                opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, opt =>
+            {
+                // Doğrulanan token’ın HttpContext üzerinde saklanmasını sağlar.
+                opt.SaveToken = true;
+                opt.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ValidateIssuerSigningKey = false,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:SecretKey"])),
+                    ValidateLifetime = false,
+                    ValidIssuer = configuration["JWT:Issuer"],
+                    ValidAudience = configuration["JWT:Audience"],
+                    ClockSkew = TimeSpan.Zero,
+                };
+            });
+        }
+    }
+}
