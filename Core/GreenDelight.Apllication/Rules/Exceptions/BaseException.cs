fe05964cbf22b72156f8967e5166b65f80eef9cb
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-11-29 19:10:20 +0300

    created baseexceptions

---
+++ Core/GreenDelight.Apllication/Rules/Exceptions/BaseException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenDelight.Application.Rules.Exceptions
+{
+    public class BaseException : ApplicationException
+    {
+        public BaseException() { }
+        public BaseException(string message) : base(message) { }
+
+    }
+}
