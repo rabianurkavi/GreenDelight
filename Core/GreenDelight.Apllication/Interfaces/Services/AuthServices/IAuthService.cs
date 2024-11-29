72fa14d9fd99636c8798fb69f30d98135d24d6b0
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-11-29 19:08:39 +0300

    Created AuthService of interface

---
+++ Core/GreenDelight.Apllication/Interfaces/Services/AuthServices/IAuthService.cs
@@ -0,0 +1,17 @@
+﻿using GreenDelight.Application.DTOs.AuthDtos.LoginDtos;
+using GreenDelight.Application.DTOs.AuthDtos.RegisterDtos;
+using GreenDelight.Application.Helpers.JWT;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenDelight.Application.Interfaces.Services.AuthServices
+{
+    public interface IAuthService
+    {
+        Task<TokenResponse> LoginAsync(LoginDto loginDto);
+        Task RegisterAsync(RegisterDto registerDto);
+    }
+}
