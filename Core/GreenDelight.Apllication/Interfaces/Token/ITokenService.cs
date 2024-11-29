260d9748c82c9258d946bd5fe44c38e6ccd13fc3
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-11-29 19:08:52 +0300

    Cerated TokenService of interface

---
+++ Core/GreenDelight.Apllication/Interfaces/Token/ITokenService.cs
@@ -0,0 +1,19 @@
+﻿using GreenDelight.Apllication.DTOs.UserDtos;
+using GreenDelight.Domain.Concrete;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenDelight.Application.Interfaces.Token
+{
+    public interface ITokenService
+    {
+        Task<JwtSecurityToken> CreateToken(User user, IList<string> roles);
+        string GenerateRefreshToken();
+        ClaimsPrincipal? GetPrincipalFromExpiredToken(string? token);
+    }
+}
