0a60d42ea87f8e39244a8d0c09cdd6342643f418
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-11-29 19:15:57 +0300

    configurations for JWT

--- Presentation/GreenDelight.API/Program.cs
+++ Presentation/GreenDelight.API/Program.cs
@@ -1,5 +1,9 @@
 using GreenDelight.Apllication.Mapping;
 using GreenDelight.Persistence;
+using GreenDelight.Infrastructure;
+using Microsoft.OpenApi.Models;
+using GreenDelight.Apllication;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -9,8 +13,38 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddApplication();
 builder.Services.AddPersistence(builder.Configuration);
+builder.Services.AddInfrastructure(builder.Configuration);
+
 MapsterConfig.RegisterMappings();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Hepsi API", Version = "v1", Description = "Hepsi APÝ swagger client." });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "'Bearer' yazýp boþluk býraktýktan sonra Token'ý girebilirsiniz \r\n\r\n Örneðin \"Bearer vr!6o%iq(!6dkflpkv5)(yx!oio!sl=0@z^6mzwu6m-gxo$yei "
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference=new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
