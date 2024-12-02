7c84648ac68b810db458ca28d7865801731f8531
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-12-02 23:53:39 +0300

    updated registerdto

--- Core/GreenDelight.Apllication/DTOs/AuthDtos/RegisterDtos/RegisterDto.cs
+++ Core/GreenDelight.Apllication/DTOs/AuthDtos/RegisterDtos/RegisterDto.cs
@@ -10,7 +10,9 @@ namespace GreenDelight.Application.DTOs.AuthDtos.RegisterDtos
     {
         public string FullName { get; set; }
         public string Email { get; set; }
+        public string UserName { get; set; }
         public string Password { get; set; }
         public string ConfirmPassword { get; set; }
+        public string PhoneNumber { get; set; }
     }
 }
