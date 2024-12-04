a8e4490ca123f750fe45b1bbfbf719479904fe32
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-12-05 02:56:28 +0300

    created logging

---
+++ Core/GreenDelight.Apllication/Interfaces/Services/Logging/ILogService.cs
@@ -0,0 +1,14 @@
+﻿using GreenDelight.Domain.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenDelight.Application.Interfaces.Services.Logging
+{
+    public interface ILogService
+    {
+        Task AddLogAsync(ErrorLog errorLog);
+    }
+}
---
+++ Infrastructure/GreenDelight.Persistence/Services/Logging/LogService.cs
@@ -0,0 +1,27 @@
+﻿using GreenDelight.Apllication.Interfaces.UnitofWorks;
+using GreenDelight.Application.Interfaces.Services.Logging;
+using GreenDelight.Domain.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenDelight.Persistence.Services.Logging
+{
+    public class LogService : ILogService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public LogService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task AddLogAsync(ErrorLog errorLog)
+        {
+            await _unitOfWork.GetGenericRepository<ErrorLog>().AddAsync(errorLog);
+            await _unitOfWork.CommitAsync();
+        }
+    }
+}
