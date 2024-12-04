26eeb0405b0094dffb3cf788322c08cfa80eb046
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-12-05 02:56:09 +0300

    added errorlog

---
+++ Core/GreenDelight.Domain/Concrete/ErrorLog.cs
@@ -0,0 +1,21 @@
+﻿using GreenDelight.Domain.Concrete.BaseEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenDelight.Domain.Concrete
+{
+    public class ErrorLog:BaseEntity
+    {
+        public string ErrorMessage { get; set; }
+        public string StackTrace { get; set; }
+        public int ErrorCode { get; set; }
+        public string ErrorDetails { get; set; }
+        public string RequestPath { get; set; }
+        public string HttpMethod { get; set; }
+        public string UserIpAddress { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
