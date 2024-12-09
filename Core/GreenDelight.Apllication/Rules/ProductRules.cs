906ed9d32e91e9b0e1a3ab905fc8e2c52f5dcce3
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-12-09 22:52:44 +0300

    created product rules

---
+++ Core/GreenDelight.Apllication/Rules/ProductRules.cs
@@ -0,0 +1,19 @@
+﻿using GreenDelight.Application.Rules.Exceptions.ProductExceptions;
+using GreenDelight.Domain.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenDelight.Application.Rules
+{
+    public class ProductRules:BaseRules
+    {
+        public Task ProductTitleMustNotBeSame(IList<Product> products,string productName)
+        {
+            if (products.Any(x => x.ProductName == productName)) throw new ProductTitleMustNotBeSameException();
+            return Task.CompletedTask;
+        }
+    }
+}
