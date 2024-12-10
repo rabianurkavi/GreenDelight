d8bbb2055824f0bd4a6c213e81cda0ccfbf7bbd3
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-12-10 03:10:54 +0300

    created productcontroller mvc

---
+++ Presentation/GreenDelight.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using GreenDelight.Application.Interfaces.Services.ProductServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GreenDelight.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var response= await _productService.GetAllAsync();
+            return View(response);
+        }
+        public async Task<IActionResult> ProductDetail(int id)
+        {
+            ViewBag.Id = id;
+            TempData["BlogId"] = id;
+            var values= await _productService.GetByIdAsync(id);
+            return View(values);    
+        }
+    }
+}
