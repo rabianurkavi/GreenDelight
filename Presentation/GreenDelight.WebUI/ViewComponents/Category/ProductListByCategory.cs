66df401df464923646649dbdaa049e498bd864cb
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-12-10 03:11:54 +0300

    created  view component of productlistbycategory

---
+++ Presentation/GreenDelight.WebUI/ViewComponents/Category/ProductListByCategory.cs
@@ -0,0 +1,20 @@
+﻿using GreenDelight.Application.Interfaces.Services.CategoryServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GreenDelight.WebUI.ViewComponents.Category
+{
+    public class ProductListByCategory:ViewComponent
+    {
+        private readonly ICategoryService _categoryService;
+        public ProductListByCategory(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var values = await _categoryService.GetAllAsync();
+            return View(values);
+        }
+
+    }
+}
