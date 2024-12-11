c5b399337df46ead92596058b2ec6e6c1e5a680a
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-12-11 03:36:21 +0300

    added interface of CommnetService

---
+++ Core/GreenDelight.Apllication/Interfaces/Services/CommentServices/ICommentService.cs
@@ -0,0 +1,18 @@
+﻿using GreenDelight.Application.DTOs.CategoryDtos;
+using GreenDelight.Application.DTOs.CommentDtos;
+using GreenDelight.Domain.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenDelight.Application.Interfaces.Services.CommentServices
+{
+    public interface ICommentService
+    {
+        Task<IResult> AddAsync(CommentAddDto commentAddDto);
+        Task<IDataResult<List<CommentDto>>> GetAllAsync(int productId);
+        Task<IResult> RemoveAsync(int productId);
+    }
+}
