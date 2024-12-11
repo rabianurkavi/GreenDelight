e2311f5e60e1a47d5a1a548e08108899b5f263b9
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-12-11 03:36:04 +0300

    added CommentDtos

---
+++ Core/GreenDelight.Apllication/DTOs/CommentDtos/CommentAddDto.cs
@@ -0,0 +1,15 @@
+﻿using GreenDelight.Domain.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenDelight.Application.DTOs.CommentDtos
+{
+    public class CommentAddDto
+    {
+        public Guid UserId { get; set; }
+        public string CommentDescription { get; set; }
+    }
+}
---
+++ Core/GreenDelight.Apllication/DTOs/CommentDtos/CommentDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenDelight.Application.DTOs.CommentDtos
+{
+    public class CommentDto
+    {
+        public string UserName { get; set; }
+        public string UserImageUrl { get; set; }
+        public string CommentDescription { get; set; }
+    }
+}
