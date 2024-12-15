3d6e1b9a41a7f9045cbf215cd8d42cf47204a813
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-12-15 20:06:35 +0300

    created commentcontroller

---
+++ Presentation/GreenDelight.WebUI/Controllers/CommentController.cs
@@ -0,0 +1,29 @@
+﻿using GreenDelight.Application.DTOs.CommentDtos;
+using GreenDelight.Application.Interfaces.Services.CommentServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GreenDelight.WebUI.Controllers
+{
+    public class CommentController : Controller
+    {
+        private readonly ICommentService _commentService;
+
+        public CommentController(ICommentService commentService)
+        {
+            _commentService = commentService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CommentAdd(int productId)
+        {
+            ViewBag.ProductId = productId;
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CommentAdd(CommentAddDto commentAddDto)
+        {
+            var comment = await _commentService.AddAsync(commentAddDto);
+            return View();
+        }
+    }
+}
