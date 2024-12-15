faf67ff63c3bd2cce6497d921c4c5d6f667d794f
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-12-15 20:05:55 +0300

    added migrations

---
+++ Infrastructure/GreenDelight.Persistence/Migrations/20241215150001_mig_add_imageurl_property.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GreenDelight.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_add_imageurl_property : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "AspNetUsers");
+        }
+    }
+}
