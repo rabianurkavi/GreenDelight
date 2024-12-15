191a5fcecb5978c43ee362edc4f74a6603fe2ae3
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-12-15 20:10:36 +0300

    CommentListByProduct view component created to see comment list by product

--- Presentation/GreenDelight.WebUI/ViewComponents/Comment/CommentListByProduct.cs
+++ Presentation/GreenDelight.WebUI/ViewComponents/Comment/CommentListByProduct.cs
@@ -1,9 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Azure;
+using GreenDelight.Application.Interfaces.Services.CommentServices;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GreenDelight.WebUI.ViewComponents.Comment
 {
     public class CommentListByProduct:ViewComponent
     {
+        //productidsini al ona göre yorumları listele
+        private readonly ICommentService _commentService;
 
+        public CommentListByProduct(ICommentService commentService)
+        {
+            _commentService = commentService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int productId)
+        {
+            var comments = await _commentService.GetAllAsync(productId);
+            if (comments.Data.Count==0)
+                ViewBag.ErrorMessage = "Henüz yorum yapılmadı. İlk yorumu siz yapın.";
+            return View(comments);
+        }
     }
 }
