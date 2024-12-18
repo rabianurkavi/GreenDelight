7a0aae473f43cff042ab56ac038e975d5fc4898a
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-12-18 04:02:25 +0300

    added userprofile info

---
+++ Presentation/GreenDelight.WebUI/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using GreenDelight.Application.DTOs.UserDtos;
+using GreenDelight.Application.Interfaces.Services.UserServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GreenDelight.WebUI.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UserProfile()
+        {
+            var userName = User.Identity.Name;
+            var user = await _userService.TakeUserInfo(userName);
+            return View(user.Data);
+        }
+        [HttpPost]
+        public async Task<IActionResult> UserProfile(UserDto userDto)
+        {
+           return View();
+        }
+
+    }
+}
