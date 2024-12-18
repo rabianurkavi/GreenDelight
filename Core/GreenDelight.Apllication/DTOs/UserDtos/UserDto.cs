7d1c5a55c03831b24b49c9b05e26f36f565e36da
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-12-18 04:00:01 +0300

    changed property

--- Core/GreenDelight.Apllication/DTOs/UserDtos/UserDto.cs
+++ Core/GreenDelight.Apllication/DTOs/UserDtos/UserDto.cs
@@ -8,12 +8,13 @@ namespace GreenDelight.Application.DTOs.UserDtos
 {
     public class UserDto:IDto
     {
-        public int ID { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public Guid ID { get; set; }
+        public string FullName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
         public bool Gender { get; set; }
+        public string PhoneNumber { get; set; }
+        public string ImageUrl { get; set; }
         public DateTime DateOfBirth { get; set; }
     }
 }
