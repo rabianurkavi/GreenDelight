88d142db873e8d4c10ff0063ba35361332cd7fd2
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-12-18 04:03:06 +0300

    viewcomponent added for user profile info

---
+++ Presentation/GreenDelight.WebUI/ViewComponents/User/LoginUserNotification.cs
@@ -0,0 +1,26 @@
+﻿using GreenDelight.Application.Interfaces.Services.AuthServices;
+using GreenDelight.Application.Interfaces.Services.UserServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GreenDelight.WebUI.ViewComponents.User
+{
+    public class LoginUserNotification : ViewComponent
+    {     
+        private readonly IUserService _userService;
+
+        public LoginUserNotification(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var userName = User.Identity.Name;
+            //username i gönderen bir metot yaz ve userın diğer özelliklerini buradan al.
+
+            var userDto= await _userService.TakeUserInformation(userName);
+            ViewBag.UserName = userDto.FullName; 
+            return View();
+        }
+    }
+}
