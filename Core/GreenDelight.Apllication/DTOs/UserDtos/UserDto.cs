9b1de93b5f9002d7e65108625376e99b9220d8ce
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-12-20 02:22:38 +0300

    gender property deleted

--- Core/GreenDelight.Apllication/DTOs/UserDtos/UserDto.cs
+++ Core/GreenDelight.Apllication/DTOs/UserDtos/UserDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,7 +13,6 @@ namespace GreenDelight.Application.DTOs.UserDtos
         public string FullName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-        public bool Gender { get; set; }
         public string PhoneNumber { get; set; }
         public string ImageUrl { get; set; }
         public DateTime DateOfBirth { get; set; }
