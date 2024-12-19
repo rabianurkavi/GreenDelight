b24b1250a4b92b05d3c80d362a8d3a1374547efb
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-12-20 02:24:09 +0300

    user profile method added

--- Presentation/GreenDelight.WebUI/Controllers/UserController.cs
+++ Presentation/GreenDelight.WebUI/Controllers/UserController.cs
@@ -1,5 +1,6 @@
 ﻿using GreenDelight.Application.DTOs.UserDtos;
 using GreenDelight.Application.Interfaces.Services.UserServices;
+using GreenDelight.WebUI.Models.ViewModel;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GreenDelight.WebUI.Controllers
@@ -21,9 +22,31 @@ namespace GreenDelight.WebUI.Controllers
             return View(user.Data);
         }
         [HttpPost]
-        public async Task<IActionResult> UserProfile(UserDto userDto)
+        public async Task<IActionResult> UserProfile(UserVM userVM)
         {
-           return View();
+            var userName = User.Identity.Name;
+            var user = await _userService.TakeUserInfo(userName);
+            if (userVM.ImageUrl != null && userVM.ImageUrl.Length > 0)
+            {
+                var extension = Path.GetExtension(userVM.ImageUrl.FileName);
+                var newImageName = Guid.NewGuid() + extension;
+                var location = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images/", newImageName);
+                using (var stream = new FileStream(location, FileMode.Create))
+                {
+                    userVM.ImageUrl.CopyTo(stream);
+                }
+
+                user.Data.ImageUrl = newImageName;
+            }
+            user.Data.FullName = userVM.FullName;
+            user.Data.PhoneNumber = userVM.PhoneNumber;
+            user.Data.Password = userVM.Password;
+            user.Data.DateOfBirth = userVM.DateOfBirth;
+            user.Data.Email = userVM.Email;
+
+            //gelen userdto verilerini gönder ve serviste gerçek verilerle eşleştirecek şekilde ayarla
+            await _userService.UpdateProfile(user.Data);
+           return RedirectToAction("UserProfile","User");
         }
 
     }
