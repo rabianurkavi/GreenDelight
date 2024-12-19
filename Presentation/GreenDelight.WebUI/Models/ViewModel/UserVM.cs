7a031b2acde1dd8bffc8f7d17a0e7e70df7638cc
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-12-20 02:24:17 +0300

    added uservm

---
+++ Presentation/GreenDelight.WebUI/Models/ViewModel/UserVM.cs
@@ -0,0 +1,13 @@
+﻿namespace GreenDelight.WebUI.Models.ViewModel
+{
+    public class UserVM
+    {
+        public Guid ID { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+        public IFormFile ImageUrl { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
