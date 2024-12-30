5aaf721e5d9a7ca8a79694a424a23ea65f8dff1b
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-12-30 20:46:46 +0300

    contactcontroller created

---
+++ Presentation/GreenDelight.WebUI/Controllers/ContactController.cs
@@ -0,0 +1,29 @@
+﻿using GreenDelight.Application.DTOs.ContactDtos;
+using GreenDelight.Application.Interfaces.Services.ContactServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GreenDelight.WebUI.Controllers
+{
+    public class ContactController : Controller
+    {
+        private IContactService _contactService;
+
+        public ContactController(IContactService contactService)
+        {
+            _contactService = contactService;
+        }
+
+        [HttpGet]
+        public IActionResult SendMessage()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> SendMessage(ContactDto contactDto)
+        {
+            await _contactService.AddAsync(contactDto);
+            ViewBag.Message = contactDto.Message;
+            return View();
+        }
+    }
+}
