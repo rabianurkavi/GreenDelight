12862ea4e784ab333d60c2a2dceb1f65b65c7e2b
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-12-30 20:46:00 +0300

    contactdto added

---
+++ Core/GreenDelight.Apllication/DTOs/ContactDtos/ContactDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenDelight.Application.DTOs.ContactDtos
+{
+    public class ContactDto
+    {
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+    }
+}
