ca7761e289b990dc087ae648feae00abc9e044f7
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-12-30 20:46:11 +0300

    added interface of contactservice

---
+++ Core/GreenDelight.Apllication/Interfaces/Services/ContactServices/IContactService.cs
@@ -0,0 +1,16 @@
+﻿using GreenDelight.Application.DTOs.ContactDtos;
+using GreenDelight.Application.DTOs.ProductDtos;
+using GreenDelight.Domain.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenDelight.Application.Interfaces.Services.ContactServices
+{
+    public interface IContactService
+    {
+        Task<IResult> AddAsync(ContactDto contactDto);
+    }
+}
