aa8ed1271d18832b69a9504e19d4a85eef1a5ed7
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-12-30 20:46:27 +0300

    new entity created

---
+++ Core/GreenDelight.Domain/Concrete/Contact.cs
@@ -0,0 +1,18 @@
+﻿using GreenDelight.Domain.Concrete.BaseEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenDelight.Domain.Concrete
+{
+    public class Contact:AuditableEntity
+    {
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+    }
+}
