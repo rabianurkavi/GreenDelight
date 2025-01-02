37e0969b1dcabfd1a5dfd9dd946fd5c0c5e8f07e
Rabia Nur Kavi <kavirabiaa@gmail.com>
2025-01-02 18:58:53 +0300

    aboutview method added

---
+++ Presentation/GreenDelight.WebUI/Controllers/AboutController.cs
@@ -0,0 +1,22 @@
+﻿using GreenDelight.Application.Interfaces.Services.AboutServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GreenDelight.WebUI.Controllers
+{
+    public class AboutController : Controller
+    {
+        private readonly IAboutService _aboutService;
+
+        public AboutController(IAboutService aboutService)
+        {
+            _aboutService = aboutService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AboutView()
+        {
+            var about = await _aboutService.GetAbout();
+            return View(about.Data);
+        }
+    }
+}
