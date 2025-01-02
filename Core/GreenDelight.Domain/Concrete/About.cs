4363a005fda564eab993d001391c175ac18e9f1f
Rabia Nur Kavi <kavirabiaa@gmail.com>
2025-01-02 18:58:08 +0300

    created about entity

---
+++ Core/GreenDelight.Domain/Concrete/About.cs
@@ -0,0 +1,18 @@
+﻿using GreenDelight.Domain.Concrete.BaseEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenDelight.Domain.Concrete
+{
+    public class About : AuditableEntity
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ImageFile { get; set; }
+        public bool Status { get; set; }
+
+    }
+}
