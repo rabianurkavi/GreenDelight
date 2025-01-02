2cbed4da6a032116565547cd89b89765d296a6ae
Rabia Nur Kavi <kavirabiaa@gmail.com>
2025-01-02 18:57:38 +0300

    interface of aboutservice created

---
+++ Core/GreenDelight.Apllication/Interfaces/Services/AboutServices/IAboutService.cs
@@ -0,0 +1,15 @@
+﻿using GreenDelight.Application.DTOs.AboutDtos;
+using GreenDelight.Domain.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenDelight.Application.Interfaces.Services.AboutServices
+{
+    public interface IAboutService
+    {
+        Task<IDataResult<AboutDto>> GetAbout();
+    }
+}
