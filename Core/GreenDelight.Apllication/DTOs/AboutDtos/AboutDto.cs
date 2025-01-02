827ed96f1b72898280e8e57d32ab136f53f8b3c0
Rabia Nur Kavi <kavirabiaa@gmail.com>
2025-01-02 18:56:57 +0300

    created about dto

---
+++ Core/GreenDelight.Apllication/DTOs/AboutDtos/AboutDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenDelight.Application.DTOs.AboutDtos
+{
+    public class AboutDto
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ImageFile { get; set; }
+        public bool Status { get; set; }
+    }
+}
