73bf4e8e5a7ee5a27099426056372e9642cece02
Rabia Nur Kavi <kavirabiaa@gmail.com>
2025-01-19 16:07:56 +0300

    added vardiyapersonelekledto

---
+++ Core/GreenDelight.Apllication/DTOs/OtherProjectDto/VardiyaDtos/VardiyaPersonelDto/VardiyaPersonelEkleDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenDelight.Application.DTOs.OtherProjectDto.VardiyaDtos.VardiyaPersonelDto
+{
+    public class VardiyaPersonelEkleDto
+    {
+        public int PersonelId { get; set; }
+        public short MasaId { get; set; }
+        public int VardiyaId { get; set; }
+        public DateTime Tarih { get; set; }
+    }
+}
