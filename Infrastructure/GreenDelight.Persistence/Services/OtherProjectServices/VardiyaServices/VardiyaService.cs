2f4c25f08c95fcca11273b560226e01686cc0fbf
Rabia Nur Kavi <kavirabiaa@gmail.com>
2025-01-19 16:09:01 +0300

    new metot added

--- Infrastructure/GreenDelight.Persistence/Services/OtherProjectServices/VardiyaServices/VardiyaService.cs
+++ Infrastructure/GreenDelight.Persistence/Services/OtherProjectServices/VardiyaServices/VardiyaService.cs
@@ -1,9 +1,11 @@
-﻿using GreenDelight.Application.DTOs.OtherProjectDto.VardiyaDtos;
+﻿using Azure.Core;
+using GreenDelight.Application.DTOs.OtherProjectDto.VardiyaDtos;
 using GreenDelight.Application.DTOs.OtherProjectDto.VardiyaDtos.GunlukVardiyaDtos;
 using GreenDelight.Application.DTOs.OtherProjectDto.VardiyaDtos.MasaDtos;
 using GreenDelight.Application.DTOs.OtherProjectDto.VardiyaDtos.MesaiDtos;
 using GreenDelight.Application.DTOs.OtherProjectDto.VardiyaDtos.PersonelDtos;
 using GreenDelight.Application.DTOs.OtherProjectDto.VardiyaDtos.SablonDtos;
+using GreenDelight.Application.DTOs.OtherProjectDto.VardiyaDtos.VardiyaPersonelDto;
 using GreenDelight.Application.Interfaces.Services.OtherProject.VardiyaServices;
 using GreenDelight.Application.Interfaces.UnitofWorks;
 using GreenDelight.Domain.Concrete.TryEntities;
@@ -29,7 +31,6 @@ namespace GreenDelight.Persistence.Services.OtherProjectServices.VardiyaServices
             var startDate = new DateTime(monthDate.Year, monthDate.Month, 1);
             var endDate = startDate.AddMonths(1).AddDays(-1);
 
-            // Şablonları alıyoruz
             var vardiyaSablonYerler = await _unitOfWork.GetGenericRepository<VardiyaSablonYer>()
                 .GetAllAsync(
                     vsy => vsy.MasaId == masaId &&
@@ -37,45 +38,64 @@ namespace GreenDelight.Persistence.Services.OtherProjectServices.VardiyaServices
                            vsy.GecerlilikBitis >= startDate,
                     include: query => query
                         .Include(vsy => vsy.Masa)
-                        .Include(vsy => vsy.Vardiya)
+                        .Include(vsy => vsy.VardiyaSablon)
+                        .ThenInclude(vs => vs.Vardiya)
                         .ThenInclude(v => v.GunlukVardiyaMasa)
                         .ThenInclude(gvm => gvm.Personel)
                 );
 
             if (!vardiyaSablonYerler.Any())
-                throw new KeyNotFoundException(" şablon bulunamadı.");
-
+                throw new KeyNotFoundException("Şablon bulunamadı.");
             var masa = vardiyaSablonYerler.First().Masa;
             if (masa == null)
-                throw new Exception("yanlış masa bilgisi.");
+                throw new Exception("Yanlış masa bilgisi.");
 
-            var sablonlar = vardiyaSablonYerler.Select(sablon => new SablonDetayDto
+            var sablonlar = vardiyaSablonYerler.Select(sablonYer =>
             {
-                SablonId = sablon.VardiyaSablonYerId,
-                SablonTanim = sablon.Tanim,
-                SablonBaslangicTarihi = sablon.GecerlilikBaslangic,
-                SablonBitisTarihi = sablon.GecerlilikBitis,
-                MaxVardiya = sablon.MaxVardiya,
-                GunlukVardiyalar = sablon.Vardiya
-                    .SelectMany(v => v.GunlukVardiyaMasa)
-                    .Where(gvm => gvm.Tarih >= startDate && gvm.Tarih <= endDate)
-                    .GroupBy(gvm => gvm.Tarih)
-                    .Select(group => new GunlukVardiyaDto
-                    {
-                        GunlukVardiyaTarih = group.Key,
-                        Vardiyalar = group.Select(gvm => new VardiyaDto
+                var sablon = sablonYer.VardiyaSablon;
+
+                var sablonStartDate = sablonYer.GecerlilikBaslangic.GetValueOrDefault().Date;
+                var sablonEndDate = sablonYer.GecerlilikBitis.GetValueOrDefault().Date;
+
+                var tarihAraligi = Enumerable.Range(0, (int)(sablonEndDate - sablonStartDate).TotalDays + 1)
+                    .Select(offset => sablonStartDate.AddDays(offset))
+                    .ToList();
+
+                var gunlukVardiyalar = tarihAraligi.Select(tarih =>
+                {
+                    var personelGrubu = sablon.Vardiya
+                        .SelectMany(v => v.GunlukVardiyaMasa)
+                        .Where(gvm => gvm.Tarih == tarih)
+                        .Select(gvm => new PersonelFiltreDto
                         {
-                            BaslangicSaat = new TimeOnly(
-                                gvm.Vardiya.BaslangicSaat.Hour, gvm.Vardiya.BaslangicSaat.Minute),
-                            BitisSaat = new TimeOnly(
-                                gvm.Vardiya.BitisSaat.Hour, gvm.Vardiya.BitisSaat.Minute),
-                            Personel = new PersonelFiltreDto
-                            {
-                                PersonelAd = gvm.Personel.Ad,
-                                PersonelSoyad = gvm.Personel.Soyad
-                            }
-                        }).ToList()
-                    }).ToList()
+                            PersonelId = gvm.Personel.PersonelId,
+                            PersonelAd = gvm.Personel.Ad,
+                            PersonelSoyad = gvm.Personel.Soyad
+                        })
+                        .ToList();
+
+                    return new GunlukVardiyaDto
+                    {
+                        GunlukVardiyaTarih = tarih,
+                        Personeller = personelGrubu
+                    };
+                }).ToList();
+
+                return new SablonDetayDto
+                {
+                    SablonId = sablonYer.VardiyaSablonId,
+                    SablonTanim = sablon.VardiyaSablonTanim,
+                    SablonBaslangicTarihi = sablonYer.GecerlilikBaslangic,
+                    SablonBitisTarihi = sablonYer.GecerlilikBitis,
+                    MaxVardiya = sablon.MaxVardiya,
+                    Vardiyalar = sablon.Vardiya.Select(v => new VardiyaDetaylariDto
+                    {
+                        Id = v.VardiyaId,
+                        BaslangicSaat = v.BaslangicSaat.ToString("HH:mm:ss"),
+                        BitisSaat = v.BitisSaat.ToString("HH:mm:ss")
+                    }).ToList(),
+                    GunlukVardiyalar = gunlukVardiyalar
+                };
             }).ToList();
 
             return new MasaDto
@@ -85,5 +105,32 @@ namespace GreenDelight.Persistence.Services.OtherProjectServices.VardiyaServices
                 Sablonlar = sablonlar
             };
         }
+
+
+        public async Task<int?> VardiyaPersonelEkleAsync(VardiyaPersonelEkleDto vardiyaPersonelEkleDto)
+        {
+            var mevcutKayit = await _unitOfWork.GetGenericRepository<GunlukVardiyaMasa>()
+                .GetAsync(x => x.MasaId == vardiyaPersonelEkleDto.MasaId && x.VardiyaId == vardiyaPersonelEkleDto.VardiyaId && x.Tarih == vardiyaPersonelEkleDto.Tarih);
+
+            if (mevcutKayit!=null)
+            {
+                return null; 
+            }
+
+            var yeniKayit = new GunlukVardiyaMasa
+            {
+                PersonelId = vardiyaPersonelEkleDto.PersonelId,
+                MasaId = vardiyaPersonelEkleDto.MasaId,
+                VardiyaId = vardiyaPersonelEkleDto.VardiyaId,
+                Tarih = vardiyaPersonelEkleDto.Tarih
+            };
+
+            await _unitOfWork.GetGenericRepository<GunlukVardiyaMasa>().AddAsync(yeniKayit);
+
+            await _unitOfWork.CommitAsync();
+
+            return vardiyaPersonelEkleDto.PersonelId; 
+        }
+
     }
 }
